15817bf0d569ff73b09da3c0b572a58f3ef6843a
gefest <gefest@me.com>
2020-12-12 20:25:27 +0300

    les 7 hw

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -45,3 +45,16 @@ class InstaTag(Insta):
 
 class InstaPost(Insta):
     pass
+
+
+class InstaUser(Insta):
+    pass
+
+
+class InstaFollow(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    user_name = scrapy.Field()
+    user_id = scrapy.Field()
+    follow_name = scrapy.Field()
+    follow_id = scrapy.Field()
